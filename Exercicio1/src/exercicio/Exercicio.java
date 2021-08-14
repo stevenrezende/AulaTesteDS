2b242433fd7425ac71d7ab902849bb6fa8cf83a9
feliperezende <felipe.rezende@etec.sp.gov.br>
2021-08-13 21:39:34 -0300

    Exercicio 1 na sexta Feira 13

---
+++ Exercicio1/src/exercicio/Exercicio.java
@@ -0,0 +1,23 @@
+package exercicio;
+import java.util.Scanner;
+public class Exercicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        double hp, lp, ha, la, qtd;
+
+        double V, R, A;
+        R = 3.2;
+        A = 4.9;
+
+        V = 3.14159 * R * R * A;
+        System.out.println("O volume da lata de óleo é de " + V + " m³");
+
+
+
+
+	}
+
+	}
+
+
